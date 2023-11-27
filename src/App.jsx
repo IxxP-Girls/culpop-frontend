b2c0c7881c483bbfa25559ad09374aa6042fe546
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-28 01:29:04 +0900

    Chore: import '@mantine/carousel/styles.css' 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { MantineProvider, createTheme } from '@mantine/core';
 import '@mantine/core/styles.css';
+import '@mantine/carousel/styles.css';
 import { Global } from '@emotion/react';
 import GlobalFonts from './styles/GlobalFonts';
 
@@ -20,7 +21,7 @@ const theme = createTheme({
   fontFamily: 'Pretendard, sans-serif',
   primaryColor: 'lime',
   colors: {
-    dark: ['#fff', '#ACAEBF', '#8C8FA3', '#666980', '#4D4F66', '#34354A', '#2B2C3D', '#121212', '#01010A', '#000000'],
+    dark: ['#fff', '#adadad', '#828282', '#6c6c6c', '#525252', '#404040', '#2c2c2c', '#121212', '#01010A', '#000000'],
   },
 });
 
