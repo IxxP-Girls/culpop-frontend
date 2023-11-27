a18b80d478461867305bb39ce6f4b52e10e358d7
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-27 20:58:33 +0900

    Feat: Footer 컴포넌트 구현

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,18 @@
+import { Flex } from '@mantine/core';
+import { GoHome, GoHeart, GoPaperAirplane, GoSearch, GoPerson } from 'react-icons/go';
+
+const Footer = () => {
+  return (
+    <Flex justify={'center'} h={'100%'} align={'center'} bg={'#2c2c2c'}>
+      <Flex w={960} justify={'space-between'}>
+        <GoHome size={25} />
+        <GoHeart size={25} />
+        <GoPaperAirplane size={25} />
+        <GoSearch size={25} />
+        <GoPerson size={25} />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Footer;
