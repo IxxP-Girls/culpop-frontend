3e3d850c26e4409b865625b2701959c1dc31b7db
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-28 00:35:00 +0900

    Feat: MainCarousel 컴포넌트 UI 구현

---
+++ src/components/Root/MainCarousel.jsx
@@ -0,0 +1,70 @@
+import Card from './Card';
+import { Carousel } from '@mantine/carousel';
+import { GrNext, GrPrevious } from 'react-icons/gr';
+
+const data = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Best forests to visit in North America',
+    date: '23.11.03 - 12.06',
+    location: 'nature',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Hawaii beaches review: better than you think',
+    date: '23.11.23 - 12.06',
+    location: 'beach',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Mountains at night: 12 best locations to enjoy the view',
+    date: '23.11.23 - 12.13',
+    location: 'nature',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Aurora in Norway: when to visit for best experience',
+    date: '23.11.24 - 12.06',
+    location: 'nature',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Best places to visit this winter',
+    date: '23.12.03 - 12.15',
+    location: 'tourism',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Active volcanos reviews: travel at your own risk',
+    date: '23.12.03 - 12.14',
+    location: 'nature',
+  },
+];
+
+const MainCarousel = () => {
+  const slides = data.map(item => (
+    <Carousel.Slide key={item.title}>
+      <Card {...item} />
+    </Carousel.Slide>
+  ));
+
+  return (
+    <Carousel
+      slideSize={'50%'}
+      loop
+      align={'start'}
+      controlSize={26}
+      nextControlIcon={<GrNext size={30} />}
+      previousControlIcon={<GrPrevious size={30} />}>
+      {slides}
+    </Carousel>
+  );
+};
+
+export default MainCarousel;
