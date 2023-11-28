11fd6436afdf8eaf39ccec27769c02d9eb6870a9
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-28 11:51:39 +0900

    Revert "Card 컴포넌트 구현"
    This reverts commit eaf0471990aa10a84382bc81282faa9b6e266d27.

--- src/components/Root/Card.jsx
+++ src/components/Root/Card.jsx
@@ -1,36 +1,33 @@
-import { Container, Flex, Text, Title, Image, Grid } from '@mantine/core';
-import { RiMapPin2Fill, RiHeartLine } from 'react-icons/ri';
+import { Paper, Text, Container } from '@mantine/core';
+import { RiMapPin2Fill } from 'react-icons/ri';
 
-const Card = () => {
+const Card = ({ image, title, date, location }) => {
   return (
-    <Grid h={200} w={470} py={30} pos={'relative'}>
-      <Grid.Col span={4}>
-        <Image
-          src="https://images.unsplash.com/photo-1579227114347-15d08fc37cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
-          h={140}
-          w={140}
-          radius={10}
-          alt="No way!"
-        />
-      </Grid.Col>
-      <Grid.Col span={7}>
-        <Flex direction={'column'}>
-          <Title order={5}>{'티퍼런스 팝업스토어 <잠시, 멈춤 5분>'}</Title>
-          <Container display={'flex'} m={0} p={0} style={{ alignItems: 'center', gap: 5 }}>
-            <RiMapPin2Fill />
-            <Text size="md" fz={14}>
-              {'서울특별시 종로구'}
-            </Text>
-          </Container>
-          <Text size="md" pos={'absolute'} fz={14} fw={700} bottom={25} right={0}>
-            {'23.12.03 - 23.12.14'}
-          </Text>
-        </Flex>
-      </Grid.Col>
-      <Grid.Col span={1}>
-        <RiHeartLine size={25} />
-      </Grid.Col>
-    </Grid>
+    <Paper
+      p="xl"
+      h={480}
+      w={480}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        backgroundImage: `url(${image})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        borderRadius: 0,
+      }}>
+      <Container pos={'fixed'} top={300} w={400}>
+        <Text fw={900} fz={24} mt={'xs'} lineClamp={1}>
+          {title}
+        </Text>
+        <Text size="md">{date}</Text>
+        <Container display={'flex'} m={0} p={0} style={{ alignItems: 'center', gap: 5 }}>
+          <RiMapPin2Fill />
+          <Text size="md">{location}</Text>
+        </Container>
+      </Container>
+    </Paper>
   );
 };
 
