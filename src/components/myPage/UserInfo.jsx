805705ba336a3ddab4214d4f11f3fcb774458d11
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-29 15:28:58 +0900

    #9 Refactor: recoil로 상태관리 제거

--- src/components/myPage/UserInfo.jsx
+++ src/components/myPage/UserInfo.jsx
@@ -1,43 +1,25 @@
-import { Divider, Flex, Grid, GridCol } from '@mantine/core';
+import { Tabs } from '@mantine/core';
 import MyInfo from './MyInfo';
 import MyInterests from './MyInterests';
-import { useRecoilState } from 'recoil';
-import tabState from '../../recoil/atom/tabState';
 
 const UserInfo = () => {
-  const [selectTab, setSelectTab] = useRecoilState(tabState);
-
   return (
-    <Flex direction={'column'} w={'100%'}>
-      <Grid w={'100%'} fz={16} fw={600} mt={15} mb={20} pb={10} py={0}>
-        <GridCol span={6} px={0}>
-          <Flex
-            h={20}
-            my={10}
-            justify={'center'}
-            style={{ color: `${selectTab === 'info' ? 'lime' : ''}` }}
-            onClick={() => setSelectTab('info')}>
-            내 정보
-          </Flex>
-          <Divider size={selectTab === 'info' ? 2 : undefined} color={selectTab === 'info' ? 'lime' : undefined} />
-        </GridCol>
-        <GridCol span={6} px={0}>
-          <Flex
-            h={20}
-            my={10}
-            justify={'center'}
-            onClick={() => setSelectTab('interests')}
-            style={{ color: `${selectTab === 'interests' ? 'lime' : ''}` }}>
-            관심팝업
-          </Flex>
-          <Divider
-            size={selectTab === 'interests' ? 2 : undefined}
-            color={selectTab === 'interests' ? 'lime' : undefined}
-          />
-        </GridCol>
-      </Grid>
-      {selectTab === 'info' ? <MyInfo /> : <MyInterests />}
-    </Flex>
+    <Tabs color="lime" defaultValue={'info'} w={'100%'}>
+      <Tabs.List mb={30}>
+        <Tabs.Tab value="info" w={'50%'}>
+          내 정보
+        </Tabs.Tab>
+        <Tabs.Tab value="interests" w={'50%'}>
+          관심 팝업
+        </Tabs.Tab>
+      </Tabs.List>
+      <Tabs.Panel value="info">
+        <MyInfo />
+      </Tabs.Panel>
+      <Tabs.Panel value="interests">
+        <MyInterests />
+      </Tabs.Panel>
+    </Tabs>
   );
 };
 
