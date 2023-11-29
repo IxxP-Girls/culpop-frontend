2dfee84bc10acb0b94faadceb17af96017d53bc9
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-29 15:28:17 +0900

    #7 Refactor: ActionIcon 추가 및 아이콘 변경

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,26 +1,29 @@
-import { Flex } from '@mantine/core';
-import { GoHome, GoHeart, GoPaperAirplane, GoSearch, GoPerson } from 'react-icons/go';
+import { ActionIcon, Flex } from '@mantine/core';
+import { TbListSearch, TbUser, TbUserHeart, TbHome, TbSend } from 'react-icons/tb';
+import { FaRegRectangleList } from 'react-icons/fa6';
 import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
-import tabState from '../../recoil/atom/tabState';
 
 const Footer = () => {
   const navigate = useNavigate();
-  const setSelectTab = useSetRecoilState(tabState);
-
-  const handleMypageClick = tab => {
-    setSelectTab(tab);
-    navigate('/mypage');
-  };
 
   return (
     <Flex justify={'center'} h={'100%'} align={'center'} bg={'dark.6'}>
-      <Flex w={960} justify={'space-between'}>
-        <GoHome size={25} onClick={() => navigate('/')} />
-        <GoHeart size={25} onClick={() => handleMypageClick('interests')} />
-        <GoPaperAirplane size={25} />
-        <GoSearch size={25} />
-        <GoPerson size={25} onClick={() => handleMypageClick('info')} />
+      <Flex w={960} justify={'space-between'} px={20}>
+        <ActionIcon variant="transparent" color="dark.0">
+          <TbHome size={30} onClick={() => navigate('/')} />
+        </ActionIcon>
+        <ActionIcon variant="transparent" color="dark.0">
+          <FaRegRectangleList size={30} />
+        </ActionIcon>
+        <ActionIcon variant="transparent" color="dark.0">
+          <TbSend size={30} />
+        </ActionIcon>
+        <ActionIcon variant="transparent" color="dark.0">
+          <TbListSearch size={30} />
+        </ActionIcon>
+        <ActionIcon variant="transparent" color="dark.0">
+          <TbUserHeart size={30} onClick={() => navigate('/mypage')} />
+        </ActionIcon>
       </Flex>
     </Flex>
   );
