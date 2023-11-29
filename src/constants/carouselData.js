cbbb3dbf17edbf3a2484ebd650975940b8466db2
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-30 02:16:15 +0900

    Refactor: 임시 캐러셀 데이터 상수로 분리

---
+++ src/constants/carouselData.js
@@ -0,0 +1,68 @@
+export const carouselData = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Best forests to visit in North America',
+    date: '23.11.03 - 12.06',
+    location: 'nature',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Hawaii beaches review: better than you think',
+    date: '23.11.23 - 12.06',
+    location: 'beach',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Mountains at night: 12 best locations to enjoy the view',
+    date: '23.11.23 - 12.13',
+    location: 'nature',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Aurora in Norway: when to visit for best experience',
+    date: '23.11.24 - 12.06',
+    location: 'nature',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Best places to visit this winter',
+    date: '23.12.03 - 12.15',
+    location: 'tourism',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    title: 'Active volcanos reviews: travel at your own risk',
+    date: '23.12.03 - 12.14',
+    location: 'nature',
+  },
+];
+
+export const tags = [
+  '가치공간',
+  '팝업스토어',
+  '브랜드',
+  '마조네',
+  '코이무이',
+  '오디에르',
+  '트리마치',
+  '디어리쉬',
+  '무르',
+  '제리쉬',
+  '더블쿼츠',
+  '러브어스유',
+  '오앨',
+  '더발론',
+  '디자이너',
+  '성수',
+  '블랙프라이데이',
+  '세일',
+  '쇼핑',
+  '의류',
+  '옷',
+];
