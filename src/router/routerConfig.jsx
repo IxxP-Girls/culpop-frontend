1b25b49afdbb4cf822561dfe8e9abf1224665ba6
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-30 02:15:49 +0900

    #13 Feat: DetailPage 경로 추가

--- src/router/routerConfig.jsx
+++ src/router/routerConfig.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import RootPage from '../pages/RootPage';
 import MyPage from '../pages/MyPage';
 import PopupsPage from '../pages/PopupsPage';
+import DetailPage from '../pages/DetailPage';
 
 const routerConfig = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const routerConfig = createBrowserRouter([
   {
     path: '/popups',
     element: <PopupsPage />,
+  },
+  {
+    path: '/popup',
+    element: <DetailPage />,
   },
 ]);
 
