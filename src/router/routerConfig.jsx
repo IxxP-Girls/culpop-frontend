db8582d7a98bb709f61ada01154d3fcaa9bffeac
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-29 15:27:28 +0900

    #9 Feat: Mypage 라우터 설정

--- src/router/routerConfig.jsx
+++ src/router/routerConfig.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import RootPage from '../pages/RootPage';
+import MyPage from '../pages/MyPage';
 
 const routerConfig = createBrowserRouter([
   {
     path: '/',
     element: <RootPage />,
+  },
+  {
+    path: '/mypage',
+    element: <MyPage />,
   },
 ]);
 
