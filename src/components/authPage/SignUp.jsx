51923b3594c0d59978ed815655dd7f3d143b0289
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-11-30 15:32:44 +0900

    #15 Feat: SignUpOnlyCulPop 컴포넌트 분리 및 유효성 검사 기능 추가 구현

--- src/components/authPage/SignUp.jsx
+++ src/components/authPage/SignUp.jsx
@@ -16,49 +16,15 @@ import {
 import { useDisclosure } from '@mantine/hooks';
 import { MdOutlineAlternateEmail } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
+import SignUpOnlyCulPop from './SignUpOnlyCulPop';
 
 const SignUp = () => {
-  const [visible, { toggle }] = useDisclosure(false);
-  const navigate = useNavigate();
-  const icon = <MdOutlineAlternateEmail style={{ width: rem(16), height: rem(16) }} />;
-
   return (
     <Flex w={'100%'} direction={'column'}>
       <Title order={2} w={'100%'} style={{ borderBottom: '1px solid' }} py={20}>
         회원가입
       </Title>
-      <Container w={500} p={40}>
-        <Flex align={'center'}>
-          <Title order={5} w={120}>
-            이메일
-          </Title>
-          <TextInput w={300} my="md" rightSectionPointerEvents="none" rightSection={icon} placeholder="Your email" />
-        </Flex>
-        <Stack gap={0}>
-          <Flex align={'center'}>
-            <Title order={5} w={120}>
-              비밀번호
-            </Title>
-            <PasswordInput
-              w={300}
-              my="md"
-              defaultValue="secret"
-              placeholder="Your password"
-              visible={visible}
-              onVisibilityChange={toggle}
-            />
-          </Flex>
-          <Flex align={'center'}>
-            <Title order={5} w={120}>
-              비밀번호 확인
-            </Title>
-            <PasswordInput w={300} my={'md'} defaultValue="secret" visible={visible} onVisibilityChange={toggle} />
-          </Flex>
-        </Stack>
-        <Button w={'100%'} size="md" my={20}>
-          회원가입
-        </Button>
-      </Container>
+      <SignUpOnlyCulPop />
       <Grid w={'100%'}>
         <Grid.Col span={4}>
           <Divider mt={12} />
