c816ae5107e7b2752f6c79b4102211dc21bb4fc4
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-12-05 12:20:38 +0900

    Feat: mantine notification 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { RouterProvider } from 'react-router-dom';
 import routerConfig from './router/routerConfig';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Global } from '@emotion/react';
 import { MantineProvider, createTheme } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import GlobalStyles from './styles/GlobalStyles';
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import '@mantine/dates/styles.css';
-import { Global } from '@emotion/react';
-import GlobalStyles from './styles/GlobalStyles';
+import '@mantine/notifications/styles.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,6 +34,7 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <MantineProvider theme={theme} defaultColorScheme="dark">
           <Global styles={GlobalStyles} />
+          <Notifications position="bottom-right" />
           <RouterProvider router={routerConfig} />
         </MantineProvider>
         <ReactQueryDevtools initialIsOpen={false} />
