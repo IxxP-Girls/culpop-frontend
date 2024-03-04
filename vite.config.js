f02b8d382445dc9e2c10c999b1a8f4a47b6af1a3
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2024-03-04 18:40:46 +0900

    Refactor: proxy 연결

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/popup': {
+        target: `http://43.202.13.38:8081`,
+        changeOrigin: true,
+      },
+    },
+  },
 });
