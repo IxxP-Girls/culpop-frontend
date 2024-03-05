ba5d6f3f3cfe62555101e2a1952d5373af161f9e
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2024-03-06 02:47:31 +0900

    Feat: getDayOfWeek 유틸함수 구현

---
+++ src/utils/getDayOfWeek.js
@@ -0,0 +1,9 @@
+const getDayOfWeek = idx => {
+  const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const targetDate = new Date();
+  targetDate.setDate(targetDate.getDate() + idx);
+
+  return weekdays[targetDate.getDay()];
+};
+
+export default getDayOfWeek;
