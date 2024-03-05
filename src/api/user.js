5784f7cc687546ab869bd4c5909cf1c23f16fcfb
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2024-03-06 02:59:17 +0900

    Feat: SignIn, SignUp 함수 구현

--- src/api/user.js
+++ src/api/user.js
@@ -1,17 +1,26 @@
 import axios from 'axios';
 
-const { CORS_SERVER_URL = '' } = import.meta.env;
+const CORS_SERVER_URL = 'https://www.culpop.shop';
+const BASE_URL = `${CORS_SERVER_URL}/users`;
 
 const fetchInterests = async sortOption => {
-  const res = await axios.get(`${CORS_SERVER_URL}/users/profile/popupLike?sort=${sortOption}`, {
+  const res = await axios.get(`${BASE_URL}/profile/popupLike?sort=${sortOption}`, {
     withCredentials: true,
   });
   return res.data;
 };
 
 const fetchProfile = async () => {
-  const { data } = await axios.get(`/users/profile`, { withCredentials: true });
+  const { data } = await axios.get(`${BASE_URL}/profile`, { withCredentials: true });
   return data;
 };
 
-export { fetchInterests, fetchProfile };
+const signUp = async data => await axios.post(`${BASE_URL}/signup`, data);
+
+const SignIn = async data => {
+  const res = await axios.post(`${BASE_URL}/login`, data);
+
+  console.log(res);
+};
+
+export { fetchInterests, fetchProfile, signUp, SignIn };
