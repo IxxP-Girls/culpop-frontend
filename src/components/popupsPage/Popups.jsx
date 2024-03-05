f6585109fe0763c6329aab64b2c4d76dd1f50b02
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2024-03-06 02:49:58 +0900

    Refactor: 리팩토링 및 서버 연결

--- src/components/popupsPage/Popups.jsx
+++ src/components/popupsPage/Popups.jsx
@@ -1,67 +1,62 @@
 import { Container, Divider, Flex, Title } from '@mantine/core';
-import Cards from '../common/Cards';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CustomButton from '../common/CustomButton';
-import { DatePickerInput } from '@mantine/dates';
-import { dates } from '../../constants/dates';
 import { regions } from '../../constants/regions';
-import useSearchPopupsQuery from '../../hooks/queries/useSearchPopupsQuery';
-import { searchPopups } from '../../constants/mockData';
+import Cards from './Cards';
+import SearchCards from './SearchCards';
+import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import getFormattedDate from '../../utils/getFormattedDate';
+import handleDateChange from '../../utils/handleDateChange';
+import { DATE_OPTIONS, DEFAULT_REGION } from '../../constants/constants';
+import DatePickerInputGroup from './DatePickerInputGroup';
+
+const CustomButtonGroup = ({ data, clicked, setClicked }) => (
+  <>
+    {data.map(item => (
+      <CustomButton key={item} data={item} clicked={clicked} setClicked={setClicked} />
+    ))}
+  </>
+);
 
 const Popups = () => {
-  const [clickDate, setClickDate] = useState('오늘');
-  const [clickRegion, setClickRegion] = useState('전체');
+  const { name } = useParams();
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const word = query.get('word');
+
+  const [clickDate, setClickDate] = useState(DATE_OPTIONS.TODAY);
+  const [clickRegion, setClickRegion] = useState(DEFAULT_REGION);
+  const [startDate, setStartDate] = useState(getFormattedDate());
+  const [endDate, setEndDate] = useState(getFormattedDate());
   const [value, setValue] = useState([null, null]);
 
-  // const { searchPopups } = useSearchPopupsQuery();
-
-  const CustomButtonGroup = (data, clicked, setClicked) =>
-    data.map(item => <CustomButton key={item} data={item} clicked={clicked} setClicked={setClicked} />);
-
-  const DatePickerInputGroup = () =>
-    clickDate === '직접선택' && (
-      <>
-        <DatePickerInput
-          w={300}
-          type="range"
-          ml={'30%'}
-          valueFormat="YYYY / MM / DD"
-          label="찾으시는 날짜를 선택하세요."
-          placeholder="Pick dates range"
-          value={value}
-          onChange={setValue}
-          clearable
-        />
-        <Divider my={15} />
-      </>
-    );
+  useEffect(() => {
+    handleDateChange(clickDate, setStartDate, setEndDate, setValue);
+  }, [clickDate]);
 
   return (
     <Flex direction={'column'} mt={20}>
-      <Flex>
-        <Container w={'100%'} m={0} p={0}>
-          <Flex align={'center'}>
-            <Title order={4} w={100}>
-              날짜
-            </Title>
-            {CustomButtonGroup(dates, clickDate, setClickDate)}
-          </Flex>
-          <Divider my={10} />
-          {DatePickerInputGroup()}
-        </Container>
-      </Flex>
-      <Flex>
-        <Container w={'100%'} m={0} p={0}>
-          <Flex align={'center'}>
-            <Title order={4} w={100}>
-              지역
-            </Title>
-            {CustomButtonGroup(regions, clickRegion, setClickRegion)}
-          </Flex>
-          <Divider my={10} />
-        </Container>
-      </Flex>
-      <Cards cards={searchPopups} />
+      <Container w={'100%'} m={0} p={0}>
+        <Flex align={'center'}>
+          <Title order={4} w={100}>
+            날짜
+          </Title>
+          <CustomButtonGroup data={Object.values(DATE_OPTIONS)} clicked={clickDate} setClicked={setClickDate} />
+        </Flex>
+        <Divider my={10} />
+        <DatePickerInputGroup clickDate={clickDate} value={value} setValue={setValue} />
+      </Container>
+      <Container w={'100%'} m={0} p={0}>
+        <Flex align={'center'}>
+          <Title order={4} w={100}>
+            지역
+          </Title>
+          <CustomButtonGroup data={regions} clicked={clickRegion} setClicked={setClickRegion} />
+        </Flex>
+        <Divider my={10} />
+      </Container>
+      {name === 'list' ? <Cards date={[clickRegion, startDate, endDate, 1]} /> : <SearchCards date={[word, 1]} />}
     </Flex>
   );
 };
