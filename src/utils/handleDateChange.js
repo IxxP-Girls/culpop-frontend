2184d6a410c6e676be8e8c9197025c081b30ca25
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2024-03-06 02:51:05 +0900

    Feat: handleDateChange 유틸함수 구현

---
+++ src/utils/handleDateChange.js
@@ -0,0 +1,17 @@
+import { DATE_OPTIONS } from '../constants/constants';
+import getFormattedDate from './getFormattedDate';
+
+const handleDateChange = (clickDate, setStartDate, setEndDate, setValue) => {
+  const dateOptionsMap = {
+    [DATE_OPTIONS.TODAY]: 0,
+    [DATE_OPTIONS.SEVEN_DAYS]: 7,
+    [DATE_OPTIONS.TWO_WEEKS]: 14,
+  };
+
+  const n = dateOptionsMap[clickDate] || 0;
+  setStartDate(getFormattedDate({ n }));
+  setEndDate(getFormattedDate({ n }));
+  setValue([null, null]);
+};
+
+export default handleDateChange;
