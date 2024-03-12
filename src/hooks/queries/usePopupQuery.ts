206953b96554305fba85255bdb770ae8d70dfbfe
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2024-03-13 00:27:01 +0900

    Feat: 사용자별 팝업 데이터 캐싱과 캐시 갱신 주기 조정

--- src/hooks/queries/usePopupQuery.ts
+++ src/hooks/queries/usePopupQuery.ts
@@ -1,15 +1,19 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { fetchPopup } from '../../api/popup';
+import { userState } from '../../recoil/atom';
+import { useRecoilValue } from 'recoil';
 
-const staleTime = 1000 * 3;
+const staleTime = 1000;
 
 const usePopupQuery = (id: number) => {
+  const email = useRecoilValue(userState);
+
   const query = useSuspenseQuery({
-    queryKey: ['@Popup', id],
+    queryKey: ['@Popup', id + '', email],
     queryFn: () => fetchPopup(id),
     staleTime,
   });
-  return { ...query, popupInfo: query.data };
+  return { popupInfo: query.data };
 };
 
 export default usePopupQuery;
