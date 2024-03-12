fa9101e71f9546aac635f6830fd72c327631512f
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2024-03-13 00:21:34 +0900

    Refactor: 변수명 수정

--- src/components/myPage/MyInterests.tsx
+++ src/components/myPage/MyInterests.tsx
@@ -5,20 +5,23 @@ import { useState } from 'react';
 
 const MyInterests = () => {
   const iconStyle = { width: rem(12), height: rem(12) };
-  const [sortOption, setSortOption] = useState('all');
+  const [sortOption, setSortOption] = useState('전체');
 
   const tabClick = (value: string) => setSortOption(value);
 
   return (
     <Tabs color="green" defaultValue="all" variant="pills">
       <Tabs.List>
-        <Tabs.Tab value="all" leftSection={<FaList style={iconStyle} />} onClick={() => tabClick('all')}>
+        <Tabs.Tab value="all" leftSection={<FaList style={iconStyle} />} onClick={() => tabClick('전체')}>
           전체
         </Tabs.Tab>
-        <Tabs.Tab value="open" leftSection={<FaSpinner style={iconStyle} />} onClick={() => tabClick('open')}>
+        <Tabs.Tab value="open" leftSection={<FaSpinner style={iconStyle} />} onClick={() => tabClick('진행 중')}>
           진행 중
         </Tabs.Tab>
-        <Tabs.Tab value="soon" leftSection={<FaRegCalendarAlt style={iconStyle} />} onClick={() => tabClick('soon')}>
+        <Tabs.Tab
+          value="soon"
+          leftSection={<FaRegCalendarAlt style={iconStyle} />}
+          onClick={() => tabClick('오픈 예정')}>
           오픈 예정
         </Tabs.Tab>
       </Tabs.List>
